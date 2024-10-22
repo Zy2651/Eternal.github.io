86bae50ddd86f3e88a7118d0379772bd14daf3d5
Manan Auluck <mananauluck@gmail.com>
2024-10-22 01:21:00 -0400

    Changes

--- update-and-deploy.bat
+++ update-and-deploy.bat
@@ -2,7 +2,7 @@
 # Save as update-and-deploy.sh (Mac/Linux) or update-and-deploy.bat (Windows)
 
 # Update the image list
-python update_images.py
+python3 update_images.py
 
 # Git commands
 git add js/main.js
