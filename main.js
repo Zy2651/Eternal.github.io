7be4358ce7c101569d95433ab5c9df8c54ed9f45
Zy2651 <dabisawesome92@gmail.com>
2024-11-10 18:50:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+document.querySelectorAll('.nav-link').forEach((link) => {
+    link.addEventListener('mouseover', () => {
+        link.classList.add('pop-out')
+    })
+    link.addEventListener('mouseout', () => {
+        link.classList.remove('pop-out')
+    })
+})
